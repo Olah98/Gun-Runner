bb6af57e3738d8c9bbdd41e38fa3f1e7282bcf98
Dylan Loe <dylanloe@gmail.com>
2020-06-07 12:39:32 -0700

    ai idle
    when not targeting, the ai can either patrol back and forth or simple move randomly

--- Gun Runner/Assets/Scripts/AI/AIPathingBase.cs
+++ Gun Runner/Assets/Scripts/AI/AIPathingBase.cs
@@ -3,9 +3,20 @@ using System.Collections.Generic;
 using UnityEngine.AI;
 using UnityEngine;
 
+
+public enum direction
+{
+    up,
+    down,
+    left,
+    right,
+    stop
+}
+
 public class AIPathingBase : MonoBehaviour
 {
     //finds player that is closer and will move towards them
+    //when there is not a player to track, it will either move in a patrol manner OR randomly up, down, left or right?
 
     Transform[] _nearbyEnemies;
 
@@ -16,21 +27,101 @@ public class AIPathingBase : MonoBehaviour
 
     public int health = 100;
 
-    private Transform _poi;
+    public Transform _poi;
+
+    public float directionDurationMax;
+    public float directionDurationMin;
+
+    public direction patrolDir;
+    direction _opposite;
+    direction _current;
+    public bool canPatrol = true;
+
+    public float speed;
+    private float _currentUsedSpeed;
+    private float _speedMin;
+    private float _speedMax;
+
+    //patrol will be either moving one direction or anthor
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        speed = this.GetComponent<NavMeshAgent>().speed;
+        _speedMin = speed / 2;
+        _speedMax = speed;
+        _currentUsedSpeed = Random.Range(_speedMin, _speedMax);
+
+        if(canPatrol)
+        {
+            directionDurationMax = 4f;
+            directionDurationMin = 3f;
+
+            //pick a random direction, and we will only use that one and its opposite to move back and forth
+            float directionNum = Random.Range(0.0f, 1.0f);
+            if (directionNum <= 0.25f)
+            {
+                patrolDir = direction.left;
+                _current = direction.left;
+                _opposite = direction.right;
+            }   
+            else if (directionNum <= 0.5f && directionNum > 0.25f)
+            {
+                patrolDir = direction.right;
+                _current = direction.right;
+                _opposite = direction.left;
+            }   
+            else if (directionNum <= 0.75f && directionNum > 0.5f)
+            {
+                patrolDir = direction.up;
+                _current = direction.up;
+                _opposite = direction.down;
+            }    
+            else if (directionNum > 0.75f)
+            {
+                patrolDir = direction.down;
+                _current = direction.down;
+                _opposite = direction.up;
+            }
+        }
+        else
+        {
+            directionDurationMax = 2f;
+            directionDurationMin = 0.5f;
+
+            //we pick a random direction (no opposite cause it gets refreshed each time
+            float directionNum = Random.Range(0.0f, 1.0f);
+            if (directionNum <= 0.25f)
+            {
+                patrolDir = direction.left;
+            }
+            else if (directionNum <= 0.5f && directionNum > 0.25f)
+            {
+                patrolDir = direction.right;
+            }
+            else if (directionNum <= 0.75f && directionNum > 0.5f)
+            {
+                patrolDir = direction.up;
+            }
+            else if (directionNum > 0.75f)
+            {
+                patrolDir = direction.down;
+            }
+        }
+
+        StartCoroutine(changeDirections());
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(_poi == null) //&& !_poi.gameObject.activeInHierarchy)
+        //if target is destoryed or doesnt exist, we check for a target
+        if(_poi == null || !_poi.gameObject.activeInHierarchy)
         {
+            patrolMovement();
             //aquire target
             _poi = GetClosestEnemy();
-            
+
         }
         else
         {
@@ -53,9 +144,8 @@ public class AIPathingBase : MonoBehaviour
         {
             if (potentialTarget.gameObject.tag == "Player")
             {
-
                 float dSqrToTarget = getDistanceSqr(currentPosition, potentialTarget.position);
-                //Debug.Log("Slinger Found target: " + dSqrToTarget);
+                
                 if (dSqrToTarget < closestDistanceSqr)
                 {
                     closestDistanceSqr = dSqrToTarget;
@@ -70,6 +160,7 @@ public class AIPathingBase : MonoBehaviour
 
     }
 
+    //locate transforms from colliders found in sphere
     private Transform[] collidersToTransforms(Collider[] colliders)
     {
         Transform[] transforms = new Transform[colliders.Length];
@@ -80,7 +171,7 @@ public class AIPathingBase : MonoBehaviour
         return transforms;
     }
 
-    //show range of enemy (mainly for debugging
+    //show range of enemy (mainly for debugging)
     private void OnDrawGizmos()
     {
         Gizmos.DrawWireSphere(transform.position, checkRadius);
@@ -91,5 +182,73 @@ public class AIPathingBase : MonoBehaviour
     {
         Vector3 directionToTarget = targetPoint - initialPoint;
         return directionToTarget.sqrMagnitude;
+    }
+
+    void patrolMovement()
+    {
+        switch (_current)
+        {
+            case direction.up:
+                //z forward
+                agent.transform.Translate(Vector3.forward * _currentUsedSpeed * Time.deltaTime);
+                break;
+            case direction.down:
+                //z backwards
+                agent.transform.Translate(Vector3.back * _currentUsedSpeed * Time.deltaTime);
+                break;
+            case direction.left:
+                //x left
+                agent.transform.Translate(Vector3.left * _currentUsedSpeed * Time.deltaTime);
+                break;
+            case direction.right:
+                //x right
+                agent.transform.Translate(Vector3.right * _currentUsedSpeed * Time.deltaTime);
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    IEnumerator changeDirections()
+    {
+        
+        yield return new WaitForSeconds(Random.Range(directionDurationMin, directionDurationMax));
+        //wait a few seconds, then change directions
+        _current = direction.stop;
+        yield return new WaitForSeconds(Random.Range(0.5f, 2f));
+
+        if (canPatrol)
+        {
+            _currentUsedSpeed = Random.Range(_speedMin, _speedMax);
+            direction temp = patrolDir;
+            patrolDir = _opposite;
+            _current = _opposite;
+            _opposite = temp;
+            
+        }
+        else
+        {
+            //we pick a random direction (no opposite cause it gets refreshed each time
+            float directionNum = Random.Range(0.0f, 1.0f);
+            if (directionNum <= 0.25f)
+            {
+                _current = direction.left;
+            }
+            else if (directionNum <= 0.5f && directionNum > 0.25f)
+            {
+                _current = direction.right;
+            }
+            else if (directionNum <= 0.75f && directionNum > 0.5f)
+            {
+                _current = direction.up;
+            }
+            else if (directionNum > 0.75f)
+            {
+                _current = direction.down;
+            }
+        }
+
+        StartCoroutine(changeDirections());
     }
 }
