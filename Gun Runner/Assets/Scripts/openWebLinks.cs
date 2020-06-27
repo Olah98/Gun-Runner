c5da07aa24a353571829129ec05ee17fdbeddfaf
Necko21 <halokid04@gmail.com>
2020-06-26 22:22:21 -0700

    Added basic UI Elements to Player
    Added in:
    -Health UI
    -Ammo UI
    -Cash UI
    -Feedback form UI
    -End Game UI
    -Cargo UI

---
+++ Gun Runner/Assets/Scripts/openWebLinks.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openWebLinks : MonoBehaviour
+{
+    public void openLink ()
+    {
+        Application.OpenURL("https://docs.google.com/forms/d/e/1FAIpQLSem9cVMePua6J2fv2-hnhtHYwm8LhxZH4pvQU_6TwdZOA_h2g/viewform");
+    }
+}
