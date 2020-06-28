6822629f3a09a755018a976455a07c5ce1c178f9
Necko21 <halokid04@gmail.com>
2020-06-27 21:25:18 -0700

    Added in Level Transition script
    A basic version of it is on the Tutorial Level

---
+++ Gun Runner/Assets/Scripts/LevelTransition.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelTransition : MonoBehaviour
+{
+    public string nextLevel;
+
+    private void loadLevel ()
+    {
+        SceneManager.LoadScene(nextLevel);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            loadLevel();
+        }
+    }
+}
