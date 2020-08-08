ecf4ac456c67ee316aaf1ff9bb951f567b4748b0
Wazzledorf <67977977+Wazzledorf@users.noreply.github.com>
2020-08-08 13:36:02 -0700

    fixed camera and ai stuff

--- Gun Runner/Assets/Scripts/Player/Player_Controller.cs
+++ Gun Runner/Assets/Scripts/Player/Player_Controller.cs
@@ -205,7 +205,7 @@ public class Player_Controller : MonoBehaviour
     void SetPlayerGunData()
     {
         //can find using item number or by its name (must be exact)
-        myBag.SetGun1(weaponData.FindWeapon("Grenade Launcher"));
+        myBag.SetGun1(weaponData.FindWeapon(1));
         myBag.SetGun2(weaponData.FindWeapon(7));
         myBag.SetCargo(weaponData.FindWeapon("Grenade Launcher"));
     }
