d558d63cdc4b3056130fda2d20d38cc6c0a18fef
Dylan Loe <dylanloe@gmail.com>
2020-08-08 16:10:59 -0700

    fixed weird bug cause from earlier mess
    this bug is killing me

--- Gun Runner/Assets/Scripts/Player/PlayerInventory.cs
+++ Gun Runner/Assets/Scripts/Player/PlayerInventory.cs
@@ -46,18 +46,18 @@ public class PlayerInventory : MonoBehaviour
     {
         slot1 = gunData;
         slot1.ammoInMag = slot1.magSize;
-        _playerWeapon.checkWeapon();
+       // _playerWeapon.checkWeapon();
     }
     public void SetGun2(WeaponInstance gunData)
     {
         slot2 = gunData;
         slot2.ammoInMag = slot2.magSize;
-        _playerWeapon.checkWeapon();
+       // _playerWeapon.checkWeapon();
     }
     public void SetCargo(WeaponInstance cargoData)
     {
         cargo = cargoData;
         cargo.ammoInMag = cargo.magSize;
-        _playerWeapon.checkWeapon();
+       // _playerWeapon.checkWeapon();
     }
 }
