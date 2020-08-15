33a8f903056de4c209845ea81e3c66e71f814990
GamerRichard96 <68299114+GamerRichard96@users.noreply.github.com>
2020-08-14 22:58:25 -0700

    Added script for keys and doors
    -Added script to collected marked keys and open marked doors
    -Applied script to Player in Levels 1 & 3 (Not addded to prefab)
    -It's a secret to everyone...

---
+++ Gun Runner/Assets/Scripts/openDoor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openDoor : MonoBehaviour
+{
+    public int keyAmount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        keyAmount = 0;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Key")
+        {
+            Destroy(other.gameObject);
+            keyAmount += 1;
+            print("Key Get");
+        }
+        if (other.gameObject.tag == "Door" && keyAmount >= 1)
+        {
+            Destroy(other.gameObject);
+            keyAmount -= 1;
+            print("Door Destroyed");
+        }
+    }
+}
